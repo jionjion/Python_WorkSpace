16f0c923a8314217545ae51ead1998cb5b2601d6
jionjion <1434501783@qq.com>
2017-10-12 23:36:32 +0800

    no message

---
+++ PythonBase/src/beautiful_soup/快速开始.py
@@ -0,0 +1,25 @@
+# 默认情况下需要升级  pip install --upgrade beautifulsoup4
+from bs4 import BeautifulSoup
+
+# 示例文档
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+# 解析
+soup = BeautifulSoup(html_doc);
+
+
+print(soup.prettify());
+
+
