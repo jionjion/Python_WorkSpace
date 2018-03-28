5bca0706992b4a15b4b4ea22b9f07e36b441efe2
jionjion <1434501783@qq.com>
2018-03-28 23:27:24 +0800

    no message

--- ArticleSpider/main.py
+++ ArticleSpider/main.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
     print('执行项目路径:',os.path.dirname(os.path.abspath(__file__)))
     # 将运行文件的位置,添加到系统执行路径中
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    # 使用命令组合,调用命令行启动程序
     execute(['scrapy', 'crawl', 'jobbole'])
